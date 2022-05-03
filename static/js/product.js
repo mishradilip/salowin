5bfff5fd7e5388201583eb793ab9e047f7c2e222
Dilip Mishra <bluestacks@Bluestackss-MacBook-Pro.local>
2022-05-03 23:26:36 +0530

    Product page share section added

--- static/js/product.js
+++ static/js/product.js
@@ -23,6 +23,35 @@ const productEvents = () => {
     },
   });
 
+  // Copy to clipboard
+  const copyTextareaBtn = document.querySelector('.js-textareacopybtn');
+  copyTextareaBtn.addEventListener('click', function(event) {
+    var copyTextarea = document.querySelector('.share-wrapper .js-copytextarea').innerHTML;
+    const textareaEle = document.querySelector('.share-wrapper .textarea-input');
+    textareaEle.value = copyTextarea;
+    textareaEle.focus();
+    textareaEle.select();
+
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Copying text command was ' + msg);
+    } catch (err) {
+      console.log('Oops, unable to copy');
+    }
+  });
+
+  // Show share block
+  const shareProduct = document.querySelector('.share-block .share-item');
+  shareProduct.addEventListener('click', function() {
+    const shareWrapper = document.querySelector('.share-block .share-wrapper');
+    if(shareWrapper.classList.contains('show')) {
+      shareWrapper.classList.remove('show');
+    } else {
+      shareWrapper.classList.add('show');
+    }
+  });
+
 };
 
 productEvents();
