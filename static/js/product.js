3403ce03ca134025ad310022e0154628554ee372
Dilip Mishra <bluestacks@Bluestackss-MacBook-Pro.local>
2022-05-03 23:54:13 +0530

    Product page changes

--- static/js/product.js
+++ static/js/product.js
@@ -52,6 +52,42 @@ const productEvents = () => {
     }
   });
 
+  // Porduct description tabs
+  const tabs = document.querySelectorAll(".product-detail-tab-container .product-tabs li");
+  tabs.forEach(element => {
+    element.addEventListener("click", function () {
+      const currentElement = element;
+      const elementID = currentElement.dataset.target;
+      const scrollElement = document.getElementById(elementID);
+      console.log('scrollElement',scrollElement.offsetTop);
+      //smoothScroll(scrollElement);
+      // scrollElement.scrollIntoView({behavior: "smooth"});
+    });
+  });
+
 };
+
+window.smoothScroll = function(target) {
+  var scrollContainer = target;
+  do { //find scroll container
+      scrollContainer = scrollContainer.parentNode;
+      if (!scrollContainer) return;
+      scrollContainer.scrollTop += 1;
+  } while (scrollContainer.scrollTop == 0);
+
+  var targetY = 0;
+  do { //find the top of target relatively to the container
+      if (target == scrollContainer) break;
+      targetY += target.offsetTop;
+  } while (target = target.offsetParent);
+
+  scroll = function(c, a, b, i) {
+      i++; if (i > 30) return;
+      c.scrollTop = a + (b - a) / 30 * i;
+      setTimeout(function(){ scroll(c, a, b, i); }, 20);
+  }
+  // start scrolling
+  scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
 
 productEvents();
